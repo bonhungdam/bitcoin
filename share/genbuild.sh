757324a078e0d062ec8d179d811ef63778bf0139
bonhungdam <167517561+bonhungdam@users.noreply.github.com>
2024-04-21 14:42:19 +0700

    Update genbuild.sh

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -17,7 +17,21 @@ else
     echo "Usage: $0 <filename> <srcroot>"
     exit 1
 fi
+GIT_TAG=""
+GIT_COMMIT=""
+if [ "${BITCOIN_GENBUILD_NO_GIT}" != "1" ] && [ -e "$(command -v git)" ] && [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
+    # clean 'dirty' status of touched files that haven't been modified
+    git diff >/dev/null 2>/dev/null
 
+    # if latest commit is tagged and not dirty, then override using the tag name
+    RAWDESC=$(git describe --abbrev=0 2>/dev/null)
+    if [ "$(git rev-parse HEAD)" = "$(git rev-list -1 "$RAWDESC" 2>/dev/null)" ]; then
+        git diff-index --quiet HEAD -- && GIT_TAG=$RAWDESC
+    fi
+
+    # otherwise generate suffix from git, i.e. string like "59887e8-dirty"
+    GIT_COMMIT=$(git rev-parse --short=12 HEAD)
+    git diff-index --quiet HEAD -- || GIT_COMMIT="$GIT_COMMIT-dirty"
 GIT_TAG=""
 GIT_COMMIT=""
 if [ "${BITCOIN_GENBUILD_NO_GIT}" != "1" ] && [ -e "$(command -v git)" ] && [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
